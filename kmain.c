e507d256001e270666403698b018eb0a4db02ab1
Henrry <henriquesantos.sje@gmail.com>
2026-02-24 22:22:08 -0300

    Formatação Para Melhor Legibilidade do kmain.c

--- kmain.c
+++ kmain.c
@@ -24,15 +24,6 @@ void serial_configure_line_control(unsigned short com);
 /* Endereço inicial da memória de vídeo para o framebuffer */
 char *fb = (char *) 0x000B8000;
 
-/** serial_is_transmit_fifo_empty:
- * Verifica se a fila de transmissão está vazia para o COM dado.
- * Retorna 0 se não estiver vazia, diferente de 0 se estiver vazia.
- */
-int serial_is_transmit_fifo_empty(unsigned int com) {
-    /* 0x20 = 0010 0000 (bit 5 da porta de status da linha) */
-    /* Você precisa somar 5 à porta base (ex: 0x3F8 + 5 = 0x3FD) */
-    return inb(com + 5) & 0x20; 
-}
 
 /** fb_write_cell:
  * Escreve um caractere com as cores de frente e fundo em uma posição i.
@@ -42,21 +33,6 @@ void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
 {
     fb[i] = c;
     fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-}
-
-/** serial_write:
- * Escreve o conteúdo do buffer 'buf' de comprimento 'len' na porta serial.
- *
- */
-void serial_write(unsigned short com, char *buf, unsigned int len) {
-    unsigned int i;
-    for (i = 0; i < len; i++) {
-        /* Espera o FIFO de transmissão ficar vazio antes de enviar o próximo byte */
-        while (!serial_is_transmit_fifo_empty(com));
-
-        /* Envia o caractere para a porta de dados */
-        outb(com, buf[i]);
-    }
 }
 
 
@@ -81,6 +57,19 @@ void fb_write(char *buf, unsigned int len){
     fb_move_cursor(len);
 }
 
+
+/** serial_is_transmit_fifo_empty:
+ * Verifica se a fila de transmissão está vazia para o COM dado.
+ * Retorna 0 se não estiver vazia, diferente de 0 se estiver vazia.
+ */
+int serial_is_transmit_fifo_empty(unsigned int com) {
+    /* 0x20 = 0010 0000 (bit 5 da porta de status da linha) */
+    /* Você precisa somar 5 à porta base (ex: 0x3F8 + 5 = 0x3FD) */
+    return inb(com + 5) & 0x20; 
+}
+
+
+
 /** serial_configure_baud_rate:
  * Define a velocidade da porta serial.
  *
@@ -101,6 +90,25 @@ void serial_configure_line_control(unsigned short com) {
     /* 0x03 = 8 bits de dados, sem paridade, 1 bit de parada */
     outb(com + 3, 0x03);
 }
+
+
+/** serial_write:
+ * Escreve o conteúdo do buffer 'buf' de comprimento 'len' na porta serial.
+ *
+ */
+void serial_write(unsigned short com, char *buf, unsigned int len) {
+    unsigned int i;
+    for (i = 0; i < len; i++) {
+        /* Espera o FIFO de transmissão ficar vazio antes de enviar o próximo byte */
+        while (!serial_is_transmit_fifo_empty(com));
+
+        /* Envia o caractere para a porta de dados */
+        outb(com, buf[i]);
+    }
+}
+
+
+
 
 /*função principal que coordena o kernel */
 void kmain()
